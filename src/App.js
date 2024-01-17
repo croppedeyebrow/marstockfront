6af9c0ab8381388e4232e0e8713f6b97661f8073
jjun11 <jok6961@gmail.com>
2024-01-17 18:22:44 +0900

    20240117/장현준/로그인, 회원가입 및 app.js 업데이트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import './App.css';
+import GlobalStyle from './utils/style/GlobalStyle';
+import MainPage from './main/MainPage';
+import FormPage from './form/FormPage';
+import SigninPage from './Sign/SigninPage';
+import SignupPage from './Sign/SignupPage';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+       <Router>
+          <Routes>
+            <Route path="/" element={<FormPage />} />
+            <Route path="/signin" element={<SigninPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            {/* <Route path="/findid" element={<FindIdPage />} /> */}
+            {/* <Route path="/findpw" element={<FindPwPage />} /> */}
+          </Routes>
+       </Router>
+
+    </>
+  );
 }
 
 export default App;
