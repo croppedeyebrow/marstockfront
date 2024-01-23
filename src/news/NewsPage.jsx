e4e17dd534f7039db9d12b20d2ac72c162ae6599
Croppedeyebrow <lee940706@gmail.com>
2024-01-23 15:57:47 +0900

    240123/뉴스페이지,어드민페이지/이재성

---
+++ src/news/NewsPage.jsx
@@ -0,0 +1,110 @@
+import GlobalStyle from "../utils/style/GlobalStyle";
+import Header from "../utils/style/Header";
+import Footer from "../utils/style/Footer";
+import InlineContainer from "../utils/style/InlineContainer";
+import newssumb from "../images/tvnewsimg.png";
+import styled from "styled-components";
+
+import {
+  NewsContainer,
+  NewsTitle,
+  NewsTopContainer,
+  Realtimezone,
+  MostViewZone,
+  TopTitile,
+  RealTimeBox,
+  TopBox,
+  BottomBox,
+  MostViewBox,
+  MostNewsName,
+  NewsUploadTime,
+  RealNewsName,
+  RealTime,
+  NewsBottomTitle,
+  NewsBottomContainer,
+  TvNewsBox,
+  NewsImgBox,
+  NewsInfoBox,
+  TvNewsTitle,
+  TvnewsInfo,
+} from "./NewsStyle";
+
+const NewsPage = () => {
+  // InlineContainer의 color = "orange" 를 입력하면 오렌지색 배경이 나오고, 공백("")인 경우는 보라색 배경이 나온다.
+
+  const newsItem = {
+    title: "소액 연체 다 갚으면 이력삭제해준다... 신용점수...",
+    info: "■ 제보하기 ▷ 전화 : 02-781-1234, 4444 ▷ 이메일 : kbs1234@kbs.co.kr ▷ 카카오톡 : 'KBS제보' 검색, 채널 추가 KBS | 2024-01-15 17:07",
+    imgSrc: newssumb,
+  };
+
+  const newsData = Array(8).fill(newsItem);
+
+  return (
+    <>
+      <Header />
+      <NewsTitle>뉴스</NewsTitle>
+      <InlineContainer
+        color="orange"
+        contents={
+          <NewsContainer>
+            <NewsTopContainer>
+              <Realtimezone>
+                <TopTitile>실시간 속보</TopTitile>
+
+                {[1, 2, 3, 4, 5].map((item, index) => (
+                  <RealTimeBox key={index}>
+                    <TopBox>
+                      미래에셋운용 '원년멤버' 최경주 부회장, 고문으로 물러난다 |
+                      <RealNewsName>뉴스1</RealNewsName>
+                    </TopBox>
+
+                    <BottomBox>
+                      미래에셋의 ‘원년 멤버’인 최경주 부회장이 일선에서 물러나
+                      고문 역할을 맡기로 했다. 김성진 사장과 김원 시장도 함께
+                      자리를 옮긴다. 15일 금... |
+                      <RealTime>2024-01-15 16:39</RealTime>
+                    </BottomBox>
+                  </RealTimeBox>
+                ))}
+              </Realtimezone>
+
+              <MostViewZone>
+                <TopTitile>가장 많이 본 뉴스</TopTitile>
+
+                {[...Array(12)].map((_, index) => (
+                  <MostViewBox key={index}>
+                    많이본뉴스 많이본뉴스많이본누스맘많이본뉴스많... |
+                    <MostNewsName>위클리 뉴스</MostNewsName>|
+                    <NewsUploadTime>2024-01-15 14:18</NewsUploadTime>
+                  </MostViewBox>
+                ))}
+              </MostViewZone>
+            </NewsTopContainer>
+
+            <NewsBottomTitle>TV뉴스</NewsBottomTitle>
+
+            <NewsBottomContainer>
+              {newsData.map((newsItem, index) => (
+                <TvNewsBox key={index}>
+                  <NewsImgBox
+                    alt="뉴스썸네일"
+                    src={newsItem.imgSrc}
+                  ></NewsImgBox>
+
+                  <NewsInfoBox>
+                    <TvNewsTitle>{newsItem.title}</TvNewsTitle>
+                    <TvnewsInfo>{newsItem.info}</TvnewsInfo>
+                  </NewsInfoBox>
+                </TvNewsBox>
+              ))}
+            </NewsBottomContainer>
+          </NewsContainer>
+        }
+      ></InlineContainer>
+      <Footer />
+    </>
+  );
+};
+
+export default NewsPage;
---
+++ src/news/NewsStyle.jsx
@@ -0,0 +1,280 @@
+import styled from "styled-components";
+
+export const NewsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 5rem;
+`;
+export const Example = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+  &.b {
+    background-color: red;
+  }
+  &.c {
+    background-color: green;
+  }
+`;
+
+export const NewsTitle = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  top: 14rem;
+  left: 5rem;
+  height: 6.3rem;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: bold;
+  font-size: 3.6rem;
+  color: #ffffff;
+`;
+
+export const NewsTopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  width: 100%;
+  height: 44.8rem;
+  border: 1px solid black;
+  gap: 2.6rem;
+`;
+
+export const Realtimezone = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 54.9rem;
+  height: 44.8rem;
+  gap: 2.4rem;
+  background: rgba(0, 0, 0, 0.4);
+  border-radius: 15px;
+`;
+
+export const MostViewZone = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 54.9rem;
+  height: 44.8rem;
+  gap: 0.5rem;
+  background: rgba(0, 0, 0, 0.4);
+  border-radius: 15px;
+`;
+
+export const TopTitile = styled.div`
+  display: flex;
+  position: relative;
+  font-size: 2.2rem;
+  color: white;
+  font-weight: bold;
+  height: 4rem;
+  width: 100%;
+  top: 1.5rem;
+  left: 1.5rem;
+`;
+
+export const RealTimeBox = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 50.7rem;
+  height: 5rem;
+
+  left: 1.5rem;
+`;
+
+export const TopBox = styled.div`
+  position: relative;
+  display: flex;
+  width: 50.7rem;
+  height: 2.9rem;
+  gap: 0.5rem;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 2.3rem;
+
+  color: #ab81ff;
+`;
+
+export const RealNewsName = styled.div`
+  position: relative;
+  display: flex;
+
+  width: 4.4rem;
+
+  height: 2.9rem;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 2.3rem;
+  color: var(--mainlightorange);
+`;
+
+export const BottomBox = styled.div`
+  position: relative;
+  display: flex;
+  width: 50.7rem;
+  height: 4.4rem;
+
+  justify-content: center;
+  align-items: center;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.3rem;
+  line-height: 1.9rem;
+
+  color: #ffffff;
+`;
+
+export const RealTime = styled.div`
+  position: relative;
+  display: flex;
+  width: 14rem;
+  height: 2.9rem;
+  align-items: center;
+
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.3rem;
+  line-height: 1.9rem;
+  color: #ffffff;
+`;
+
+export const MostViewBox = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 51.7rem;
+  height: 2.8rem;
+
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.3rem;
+  line-height: 2rem;
+  left: 1.5rem;
+  color: #ffffff;
+`;
+
+export const MostNewsName = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 7.2rem;
+  height: 2.8rem;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.3rem;
+  line-height: 2rem;
+  color: var(--mainlightorange);
+`;
+
+export const NewsUploadTime = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 12.2rem;
+  height: 2.8rem;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.3rem;
+  line-height: 2rem;
+  color: #ffffff;
+`;
+
+export const NewsBottomTitle = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 3.4rem;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 2.2rem;
+  color: #ffffff;
+  top: 3rem;
+  left: 1.2rem;
+  margin-bottom: 4rem;
+`;
+
+export const NewsBottomContainer = styled.div`
+  display: grid;
+  position: relative;
+  border: 1px solid black;
+  width: 100%;
+  height: 100%;
+  gap: 2rem;
+  grid-template-columns: repeat(2, 54rem);
+  grid-template-rows: repeat(3, 12.5rem);
+`;
+
+export const TvNewsBox = styled.div`
+  position: relative;
+  display: flex;
+  width: 54rem;
+  height: 12.5rem;
+  gap: 1.8rem;
+  align-items: center;
+  justify-content: center;
+
+  background: rgba(255, 184, 136, 0.12);
+  border-radius: 15px;
+`;
+
+export const NewsImgBox = styled.img`
+  display: flex;
+  position: relative;
+  width: 14.8rem;
+  height: 8.3rem;
+  border: 1px solid black;
+`;
+
+export const NewsInfoBox = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 0.5rem;
+  width: 34rem;
+  height: 8.3rem;
+  border: 1px solid white;
+`;
+
+export const TvNewsTitle = styled.div`
+  display: flex;
+  position: relative;
+  width: 34rem;
+  height: 2.4rem;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1.5rem;
+  color: var(--mainlightorange);
+`;
+
+export const TvnewsInfo = styled.div`
+  display: flex;
+  position: relative;
+  width: 34rem;
+  height: 2.4rem;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: lighter;
+  font-size: 1.1rem;
+  color: #ffffff;
+`;
