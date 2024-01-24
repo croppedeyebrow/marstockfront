431220b3f34870acdd4e24d01ef6e16a970710df
Croppedeyebrow <lee940706@gmail.com>
2024-01-24 20:50:19 +0900

    0124/주식정보페이지 css/이재성

--- src/stock/StockInfoPage.jsx
+++ src/stock/StockInfoPage.jsx
@@ -4,8 +4,10 @@ import Footer from "../utils/style/Footer";
 import InlineContainer from "../utils/style/InlineContainer";
 import styled from "styled-components";
 import React, { useState } from "react";
+import stockgraph from "../images/stockgraph.png";
 
 import {
+  StockInfoPageContainer,
   StockListContainer,
   StockHeadTitle,
   StockHeadTitle01,
@@ -17,7 +19,26 @@ import {
   Category03,
   Category04,
   StockInfoBackboard,
-  StockDiv,
+  StockDivLeft,
+  StockDivRight,
+  StockGraphZone,
+  CurrentPrice,
+  CurrentPriceNum,
+  UpDownCheck,
+  Point,
+  Percent,
+  LeftInfo,
+  LeftInfoTitle,
+  LeftInfoNum,
+  PurchaseBox,
+  StockPurchaseBox,
+  StockSellBox,
+  PurchaseTitle,
+  SellTitle,
+  PurchaseTop,
+  PurchaseButton,
+  SellTop,
+  SellButton,
 } from "./StockInfoStyle";
 
 const StockInfoPage = () => {
@@ -35,33 +56,127 @@ const StockInfoPage = () => {
   return (
     <>
       <Header />
-      <StockHeadTitle>
-        <StockHeadTitle01 onClick={handleHeadTitleClick}>주식</StockHeadTitle01>
-        <StockHeadTitle02 onClick={handleHeadTitleClick}>
-          종목토론
-        </StockHeadTitle02>
-        <StockHeadTitle03 onClick={handleHeadTitleClick}>
-          시장지표
-        </StockHeadTitle03>
-      </StockHeadTitle>
+      <StockInfoPageContainer>
+        <StockHeadTitle>
+          <StockHeadTitle01 onClick={handleHeadTitleClick}>
+            주식
+          </StockHeadTitle01>
+          <StockHeadTitle02 onClick={handleHeadTitleClick}>
+            종목토론
+          </StockHeadTitle02>
+          <StockHeadTitle03 onClick={handleHeadTitleClick}>
+            시장지표
+          </StockHeadTitle03>
+        </StockHeadTitle>
 
-      <InlineContainer
-        color=""
-        contents={
-          <StockListContainer>
-            <StockCategory>
-              <Category01>종목명</Category01>
-              <Category02>종목코드</Category02>
-              <Category03>KOSPI</Category03>
-              <Category04>날짜</Category04>
-            </StockCategory>
+        <InlineContainer
+          color=""
+          contents={
+            <StockListContainer>
+              <StockCategory>
+                <Category01>종목명</Category01>
+                <Category02>종목코드</Category02>
+                <Category03>KOSPI</Category03>
+                <Category04>날짜</Category04>
+              </StockCategory>
 
-            <StockInfoBackboard>
-              <StockDiv></StockDiv>
-            </StockInfoBackboard>
-          </StockListContainer>
-        }
-      ></InlineContainer>
+              <StockInfoBackboard>
+                <StockDivLeft>
+                  <CurrentPrice>
+                    현재가
+                    <CurrentPriceNum>11,000</CurrentPriceNum>
+                  </CurrentPrice>
+
+                  <UpDownCheck>
+                    <Point>▲1,000</Point>
+                    <Percent>+10.00%</Percent>
+                  </UpDownCheck>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>전일가</LeftInfoTitle>
+                    <LeftInfoNum>10,000</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>고가</LeftInfoTitle>
+                    <LeftInfoNum>13,800</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>저가</LeftInfoTitle>
+                    <LeftInfoNum>9,280</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>시가</LeftInfoTitle>
+                    <LeftInfoNum>9,980</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>거래량</LeftInfoTitle>
+                    <LeftInfoNum>17,482,300</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>거래대금</LeftInfoTitle>
+                    <LeftInfoNum>174,823 백만</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>시가총액</LeftInfoTitle>
+                    <LeftInfoNum>8,241 억원</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>시총순위</LeftInfoTitle>
+                    <LeftInfoNum>코스피 298위</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>상장주수</LeftInfoTitle>
+                    <LeftInfoNum>74,918,182</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>액면가</LeftInfoTitle>
+                    <LeftInfoNum>4,500</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>52주 최고</LeftInfoTitle>
+                    <LeftInfoNum>11,000</LeftInfoNum>
+                  </LeftInfo>
+
+                  <LeftInfo>
+                    <LeftInfoTitle>52주 최저</LeftInfoTitle>
+                    <LeftInfoNum>3,387</LeftInfoNum>
+                  </LeftInfo>
+                </StockDivLeft>
+
+                <StockDivRight>
+                  <StockGraphZone alt="주식그래프" src={stockgraph} />
+
+                  <PurchaseBox>
+                    <StockPurchaseBox>
+                      <PurchaseTop>
+                        <PurchaseTitle>매도</PurchaseTitle>
+                        <PurchaseButton>매도</PurchaseButton>
+                      </PurchaseTop>
+                    </StockPurchaseBox>
+
+                    <StockSellBox>
+                      <SellTop>
+                        <SellTitle>매수</SellTitle>
+                        <SellButton>매수</SellButton>
+                      </SellTop>
+                    </StockSellBox>
+                  </PurchaseBox>
+                </StockDivRight>
+              </StockInfoBackboard>
+            </StockListContainer>
+          }
+        ></InlineContainer>
+      </StockInfoPageContainer>
       <Footer />
     </>
   );
--- src/stock/StockInfoStyle.jsx
+++ src/stock/StockInfoStyle.jsx
@@ -21,14 +21,23 @@ export const Example = styled.div`
   }
 `;
 
+export const StockInfoPageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid white;
+`;
+
 export const StockHeadTitle = styled.div`
   display: flex;
   position: relative;
   flex-direction: row;
-  gap: 1.5rem;
-  width: 100%;
-  height: 7rem;
 
+  align-items: center;
+  border: 1px solid white;
+  gap: 1.5rem;
+  width: 50%;
+  height: 7rem;
+  left: 36rem;
   top: 14rem;
 `;
 
@@ -143,19 +152,241 @@ export const Category04 = styled.div`
 export const StockInfoBackboard = styled.div`
   position: relative;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   height: 113.4rem;
   width: 113.4rem;
   background: rgba(255, 255, 255, 0.1);
   border-radius: 15px;
 `;
 
-export const StockDiv = styled.div`
+export const StockDivLeft = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 28.3rem;
+  height: 100%;
+
+  align-items: center;
+  padding-top: 5rem;
+  gap: 2rem;
+`;
+
+export const CurrentPrice = styled.div`
   display: flex;
   position: relative;
   flex-direction: row;
-  width: 100%;
-  height: 5rem;
+  width: 24rem;
+  height: 6rem;
+
+  color: white;
+  font-size: 2rem;
+  font-weight: bold;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const CurrentPriceNum = styled.div`
+  display: flex;
+  position: relative;
+  width: 15.7rem;
+  height: 5.8rem;
+
+  font-size: 4rem;
+  font-weight: bold;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const UpDownCheck = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  width: 24rem;
+  height: 6rem;
+
+  color: var(--upred);
+  font-size: 2rem;
+  font-weight: bold;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const Point = styled.div`
+  display: flex;
+  position: relative;
+  width: 10.7rem;
+  height: 2.7rem;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Percent = styled.div`
+  display: flex;
+  position: relative;
+  width: 10.7rem;
+  height: 2.7rem;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LeftInfo = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  width: 24rem;
+  height: 4rem;
+  color: white;
 
   align-items: center;
+  justify-content: center;
+`;
+
+export const LeftInfoTitle = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+
+  width: 9.7rem;
+  height: 4rem;
+  color: white;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: lighter;
+`;
+
+export const LeftInfoNum = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+
+  width: 14.8rem;
+  height: 4rem;
+  color: white;
+  align-items: center;
+  justify-content: right;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const StockDivRight = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 84.9rem;
+  height: 100%;
+
+  align-items: center;
+  padding-top: 7rem;
+
+  gap: 6rem;
+`;
+
+export const StockGraphZone = styled.img`
+  display: flex;
+  position: relative;
+  width: 81.7rem;
+  height: 36.6rem;
+`;
+
+export const PurchaseBox = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 40rem;
+  flex-direction: row;
+  gap: 2rem;
+  margin-top: 2rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StockPurchaseBox = styled.div`
+  position: relative;
+  display: flex;
+  width: 44rem;
+  height: 44rem;
+
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  padding: 5rem 3rem;
+`;
+
+export const StockSellBox = styled.div`
+  display: flex;
+  position: relative;
+  width: 33.7rem;
+  height: 44rem;
+
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  padding: 5rem 3rem;
+`;
+
+export const PurchaseTop = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 5.2rem;
+  flex-direction: row;
+`;
+
+export const PurchaseTitle = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 5.2rem;
+  color: white;
+  font-size: 3rem;
+  font-weight: bold;
+`;
+
+export const PurchaseButton = styled.button`
+  display: flex;
+  position: relative;
+  width: 14rem;
+  height: 5.2rem;
+  color: white;
+  background: var(--mainorange);
+  font-size: 2.5rem;
+  font-weight: bold;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.2); // 가장자리를 깎는 효과 추가
+  border-radius: 0.5rem;
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2), 0px 10px 10px rgba(0, 0, 0, 0.15); // 그림자 효과 조정
+`;
+
+export const SellTitle = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 5.2rem;
+  color: white;
+  font-size: 3rem;
+  font-weight: bold;
+`;
+
+export const SellTop = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 5.2rem;
+  flex-direction: row;
+`;
+
+export const SellButton = styled.button`
+  display: flex;
+  position: relative;
+  width: 14rem;
+  height: 5.2rem;
+  color: white;
+  background: var(--mainpurple);
+  font-size: 2.5rem;
+  font-weight: bold;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.2); // 가장자리를 깎는 효과 추가
+  border-radius: 0.5rem;
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2), 0px 10px 10px rgba(0, 0, 0, 0.15); // 그림자 효과 조정
 `;
