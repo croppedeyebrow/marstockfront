1e7f8d67776c51cf2bfb4b9fb15c3c25d908df5e
hubjh <haha1057@naver.com>
2024-02-05 12:26:52 +0900

    20240205/황준호/웹소켓 수정 1

--- src/utils/common/WebSocket.jsx
+++ src/utils/common/WebSocket.jsx
@@ -1,47 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 const WebSocketComponent = () => {
-  const [receivedData, setReceivedData] = useState([]);
-
   useEffect(() => {
-    // WebSocket 연결
-    const socket = new WebSocket("ws://localhost:8111/ws/marstock");
+    const roomId = "mypage";
+    const socket = new WebSocket(
+      `ws://localhost:8111/ws/marstock?room=${roomId}`
+    );
 
-    // 연결 이벤트 핸들링
     socket.onopen = () => {
       console.log("WebSocket connected");
     };
 
-    // 메시지 수신 이벤트 핸들링
     socket.onmessage = (event) => {
       const message = event.data;
-      console.log("Received WebSocket message:", message);
-
-      // 받은 데이터를 상태로 업데이트
-      setReceivedData((prevData) => [...prevData, message]);
+      console.log(message);
     };
 
-    // 연결 종료 이벤트 핸들링
     socket.onclose = () => {
       console.log("WebSocket closed");
     };
 
-    // 컴포넌트 언마운트 시 WebSocket 연결 종료
     return () => {
       socket.close();
     };
   }, []);
 
-  return (
-    <div>
-      {/* <h2>Received Data:</h2>
-      <ul>
-        {receivedData.map((data, index) => (
-          <li key={index}>{data}</li>
-        ))}
-      </ul> */}
-    </div>
-  );
+  // 스타일 객체를 만들어 색상을 지정
+  const textStyle = {
+    color: "red", // 적용하고 싶은 색상을 지정
+  };
+
+  return <div style={textStyle}>WebSocket Component</div>;
 };
 
 export default WebSocketComponent;
